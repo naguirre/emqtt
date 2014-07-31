f0d27b468ff15eba29a308fa6f072a7bb1afcb8f
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2014-07-31 23:59:08 +0200

    Ordering and formatting EMQtt.h

--- src/lib/EMqtt.h
+++ src/lib/EMqtt.h
@@ -61,6 +61,9 @@ extern "C" {
 #define EMQTT_MAX_RETRY 6
 #define EMQTT_TIMEOUT_CONNECT 5
 
+typedef struct _EMqtt_Sn_Client EMqtt_Sn_Client;
+typedef struct _EMqtt_Sn_Server EMqtt_Sn_Server;
+
 enum _EMQTT_SN_CONNECTION_STATE
 {
     CONNECTION_CLOSED,
@@ -84,23 +87,32 @@ enum _EMQTT_SN_ERROR_TYPE
     ERROR
 };
 typedef enum _EMQTT_SN_ERROR_TYPE EMQTT_SN_ERROR_TYPE;
-typedef struct _EMqtt_Sn_Server EMqtt_Sn_Server;
-typedef struct _EMqtt_Sn_Client EMqtt_Sn_Client;
 
 typedef void (*EMqtt_Sn_Client_Connect_Cb) (void *data, EMqtt_Sn_Client *client, EMQTT_SN_CONNECTION_STATE connection_state);
 typedef void (*EMqtt_Sn_Client_Topic_Received_Cb) (void *data, EMqtt_Sn_Client *client, const char *topic, const char *value);
 typedef void (*EMqtt_Sn_Client_Subscribe_Error_Cb) (void *data, EMQTT_SN_ERROR_TYPE state);
 typedef void (*EMqtt_Sn_Client_Suback_Received_Cb) (void *data, EMqtt_Sn_Client *client);
 
+  /* General */
+
 EAPI int emqtt_init(void);
 EAPI int emqtt_shutdown(void);
+
+  /* Server/Broker */
+
 EAPI EMqtt_Sn_Server *emqtt_sn_server_add(char *addr, unsigned short port, unsigned char gw_id);
-EAPI void emqtt_sn_server_del(EMqtt_Sn_Server *srv);
+EAPI void             emqtt_sn_server_del(EMqtt_Sn_Server *srv);
+
+  /* Client */
+
 EAPI EMqtt_Sn_Client *emqtt_sn_client_add(char *addr, unsigned short port, char *client_name);
-EAPI void emqtt_sn_client_connect_send(EMqtt_Sn_Client *client, EMqtt_Sn_Client_Connect_Cb connected_cb, void *data, double keepalive);
-EAPI void emqtt_sn_client_subscribe(EMqtt_Sn_Client *client, const char *topic_name, EMqtt_Sn_Client_Topic_Received_Cb topic_received_cb, EMqtt_Sn_Client_Subscribe_Error_Cb subscribe_error_cb, void *data);
-EAPI void emqtt_sn_client_del(EMqtt_Sn_Client *srv);
-EAPI void emqtt_sn_client_send_publish(EMqtt_Sn_Client *client, const char *topic_name, const char *publish_data, EMqtt_Sn_Client_Suback_Received_Cb suback_received_cb, void *data);
+EAPI void             emqtt_sn_client_del(EMqtt_Sn_Client *srv);
+
+EAPI void             emqtt_sn_client_connect_send(EMqtt_Sn_Client *client, EMqtt_Sn_Client_Connect_Cb connected_cb, void *data, double keepalive);
+
+EAPI void             emqtt_sn_client_subscribe(EMqtt_Sn_Client *client, const char *topic_name, EMqtt_Sn_Client_Topic_Received_Cb topic_received_cb, EMqtt_Sn_Client_Subscribe_Error_Cb subscribe_error_cb, void *data);
+
+EAPI void             emqtt_sn_client_send_publish(EMqtt_Sn_Client *client, const char *topic_name, const char *publish_data, EMqtt_Sn_Client_Suback_Received_Cb suback_received_cb, void *data);
 
 #ifdef __cplusplus
 }
